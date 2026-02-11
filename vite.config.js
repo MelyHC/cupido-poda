f1e1d4e9173b77cfba62f48f3d4a75c9d8775e0a
MelyHC <mely.hidalgo.crespo@gmail.com>
2026-02-10 23:59:17 -0500

    Configuración para PWA

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
+const manifestIcons = [
+  {
+    src: 'pwa-192.png',
+    sizes: '192x192',
+    type: 'image/png',
+  },
+  {
+    src: 'pwa-512.png',
+    sizes: '512x512',
+    type: 'image/png',
+  }
+]
+
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Trust Toke',
+        short_name: 'Trust Toke',
+        icons: manifestIcons,
+      }
+    })
+  ]
 })
