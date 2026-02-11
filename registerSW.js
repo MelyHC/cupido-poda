6b4eab39e1cbbc3f9eac4a1f85a1f5d927ad21eb
MelyHC <mely.hidalgo.crespo@gmail.com>
2026-02-11 15:52:04 -0500

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('https://melyhc.github.io/cupido-poda/sw.js', { scope: 'https://melyhc.github.io/cupido-poda/' })})}
