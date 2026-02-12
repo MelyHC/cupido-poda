a0a68753fa239e75e185d2f29a21bc0d2a8d8652
MelyHC <mely.hidalgo.crespo@gmail.com>
2026-02-12 14:56:08 -0500

    Ajuste css para secciones del home

--- src/sections/WhatIs.jsx
+++ src/sections/WhatIs.jsx
@@ -3,19 +3,19 @@ import { Container } from 'react-bootstrap';
 const WhatIs = () => {
   return (
     <Container>
-      <h4 className='my-5'>¿Qué es Trust Toke?</h4>
+      <h4 className='my-5'>¿Qué es <span className='text-green'>Trust Toke</span>?</h4>
       <p>
-        Trust Toke no es dinero.
+        <span className='text-green'>Trust Toke</span> no es dinero.
         <br />No es un puntaje.
         <br />No es un trofeo.
       </p>
       <p>
         <strong>
-          Es una señal on-chain que registra un acto consciente:
+          Es una <span className='text-pink'>señal on-chain</span> que registra un <span className='text-green'>acto consciente</span>:
           participar, reconocer o aprender.
         </strong>
       </p>
-      <p>Trust Toke incluye:</p>
+      <p><span className='text-green'>Trust Toke</span> incluye:</p>
       <ul>
         <li>Emisor</li>
         <li>Receptor</li>
@@ -25,7 +25,7 @@ const WhatIs = () => {
       </ul>
       <p>
         No se transfiere. No se vende. No se farmea trivialmente.
-        <br />Existe para recordar.
+        <br />Existe para <span className='text-pink'>recordar</span>.
       </p>
     </Container>
   )
